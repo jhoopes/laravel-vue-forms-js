5155d75af8e51323ef4a14dcc47acac95b638c56
John Hoopes <john.z.hoopes@gmail.com>
2018-11-29 13:24:37 -0600

    fixing conditionals to use 'valueField' for the conditional field finder and other updates to store things in a consistent manner.  Also fixes conditionals for sub-records

--- src/mixins/UpdatesValuesAndConditions.js
+++ src/mixins/UpdatesValuesAndConditions.js
@@ -1,3 +1,5 @@
+import {assignOnObject, byString} from "../utilities/utils";
+
 export default {
 
     data() {
@@ -17,15 +19,20 @@ export default {
 
             let fieldExtra = this.getFormFieldFieldExtra(field);
 
-            if(fieldExtra.condition && fieldExtra.condition.fieldName && fieldExtra.condition.fieldValue) {
+            if(fieldExtra.condition && fieldExtra.condition.valueField && fieldExtra.condition.fieldValue) {
 
-                let conditionField = this.form.formConfig.fields.filter(f => {return f.name === fieldExtra.condition.fieldName})[0];
+                let conditionField = this.form.formConfig.fields.filter(f => {return f.value_field === fieldExtra.condition.valueField})[0];
                 var conditionFieldFieldExtra = null;
                 if(conditionField) {
                     conditionFieldFieldExtra = this.getFormFieldFieldExtra(conditionField);
+                } else {
+                    // if the valueField from the condition was not found set the view to be false
+                    this.$set(this.conditionValues, field.name, false);
+                    console.log('Invalid condition field.  Is your configuration correct?');
+                    return;
                 }
 
-                if (this.form.formFieldOptions[fieldExtra.condition.fieldName]) {
+                if (this.getConditionOptions(fieldExtra.condition.valueField)) {
 
                     if(!conditionFieldFieldExtra.options_config.optionLabelField) {
                         conditionFieldFieldExtra.options_config.optionLabelField = 'name';
@@ -35,17 +42,16 @@ export default {
                         conditionFieldFieldExtra.options_config.optionValueField = 'id';
                     }
 
-
-                    let conditionOption = this.form.formFieldOptions[fieldExtra.condition.fieldName].filter(option => {
+                    let conditionOption = this.getConditionOptions(fieldExtra.condition.valueField).filter(option => {
                         return option[conditionFieldFieldExtra.options_config.optionLabelField] === fieldExtra.condition.fieldValue
                     })[0];
 
-                    if( conditionOption && this.form.data[conditionField.value_field] === conditionOption[conditionFieldFieldExtra.options_config.optionValueField]) {
+                    if( conditionOption && this.getFieldValue(this.form.data, conditionField) === conditionOption[conditionFieldFieldExtra.options_config.optionValueField]) {
                         this.$set(this.conditionValues, field.name, true);
                     }else {
                         this.$set(this.conditionValues, field.name, false);
                     }
-                } else if(this.form.data[fieldExtra.condition.fieldName] === fieldExtra.condition.fieldValue) {
+                } else if(this.getFieldValue(this.form.data, conditionField) === fieldExtra.condition.fieldValue) {
                     this.$set(this.conditionValues, field.name, true);
                 }else {
                     this.$set(this.conditionValues, field.name, false);
@@ -66,8 +72,11 @@ export default {
         updateValueAndConditionals(newVal, field) {
             this.updateFormValue(field, newVal); this.generateConditionValues();
         },
+        getConditionOptions(valueField) {
+            return byString(this.form.formFieldOptions, valueField);
+        },
         updateOptionsForField(newOptions, field) {
-            this.$set(this.form.formFieldOptions, field.name, newOptions);
+            assignOnObject(this.form.formFieldOptions, field.value_field, newOptions);
             this.generateConditionValues();
         },
     }
