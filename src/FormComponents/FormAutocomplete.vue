c8a67cc469409da7494906ffb0ef293f12559e69
John Hoopes <john.z.hoopes@gmail.com>
2019-06-09 16:19:49 -0500

    adding ability to have get parameters with form fields that use options

--- src/FormComponents/FormAutocomplete.vue
+++ src/FormComponents/FormAutocomplete.vue
@@ -104,7 +104,13 @@
                         let fieldValue = get(this.form, match[1], '');
                         optionsURL = optionsURL.replace(match[0], fieldValue);
                     });
-                    optionsURL += '?q=' + this.search;
+
+                    var params = JSON.parse(JSON.stringify(this.fieldConfig.optionsUrlParams));
+                    params.q = this.search;
+
+                    let queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
+
+                    optionsURL += '?' + queryString;
                     return optionsURL;
                 }
                 return '';
