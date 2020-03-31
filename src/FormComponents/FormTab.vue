3db70308e1020543dfe687d97fe5acc5de3f5b20
John Hoopes <john.z.hoopes@gmail.com>
2020-03-31 00:02:42 -0500

    updating saving indicator for forms that use tabs

--- src/FormComponents/FormTab.vue
+++ src/FormComponents/FormTab.vue
@@ -18,7 +18,8 @@
                 :children="field.children || null"
             ></component>
             <div class="controls-row" v-if="disabled === false && autoSave === false">
-                <button class="button" v-for="action in actions" @click.prevent="runAction(action.action)">{{ action.label }}</button>
+                <button class="button" v-for="action in actions" @click.prevent="runAction(action.action)" v-html="action.label":disabled="showSaving && saving"></button>
+                <span v-if="saving && showSaving"><font-awesome-icon :icon="spinner" :spin="true"></font-awesome-icon>{{ savingText }}</span>
             </div>
         </div>
     </section>
@@ -28,12 +29,23 @@
     import FormField from './../mixins/FormField'
     import FormConfig from './../mixins/FormConfig'
     import UpdatesValuesAndConditions from './../mixins/UpdatesValuesAndConditions';
+
+    import {
+        faSpinner,
+    } from "@fortawesome/free-solid-svg-icons";
+
+    import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
     export default {
 
 
         mixins: [FormField, FormConfig, UpdatesValuesAndConditions],
 
         name: 'form-tab',
+
+        components: {
+            FontAwesomeIcon
+        },
 
         props: {
             children: {
@@ -65,6 +77,15 @@
             },
             actions: {
                 required: true,
+            },
+            saving: {
+                required: true,
+            },
+            showSaving: {
+                required: true,
+            },
+            savingText: {
+                required: true,
             }
         },
 
@@ -87,6 +108,7 @@
 
         data: () => ({
             isActive: false,
+            spinner: faSpinner
         }),
 
         computed: {
